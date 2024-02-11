c3f856e7a1c913e2b4d5b8fbde84e10d6d1774cf
Shvetsov Dmitry <40828643+DmitryShvetsov@users.noreply.github.com>
2024-02-11 16:14:30 +0300

    Update week3.sql

--- week3.sql
+++ week3.sql
@@ -1,3 +1,14 @@
-Question 1:
+Question 1
 SELECT count(*) FROM `de-ibphilippov.de_shvetsov.week3_green_taxi_2022` 
 
+Q2
+SELECT count(distinct(PULocationID)) FROM `de-ibphilippov.de_shvetsov.week3_green_taxi_2022` 
+
+Q3
+SELECT count(*) FROM `de-ibphilippov.de_shvetsov.week3_green_taxi_2022` where fare_amount = 0
+
+Q4
+create table
+
+Q5
+SELECT distinct(PULocationID) FROM `de-ibphilippov.de_shvetsov.week3_green_taxi_2022` where cast(lpep_pickup_datetime as date) between "2022-06-01" and "2022-06-30"
