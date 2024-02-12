283a9f3a2a3520e8dc1d075ce0973f4ce194939d
Shvetsov Dmitry <40828643+DmitryShvetsov@users.noreply.github.com>
2024-02-12 09:43:46 +0300

    Update week3.sql

--- week3.sql
+++ week3.sql
@@ -8,7 +8,14 @@ Q3
 SELECT count(*) FROM `de-ibphilippov.de_shvetsov.week3_green_taxi_2022` where fare_amount = 0
 
 Q4
-create table
+CREATE TABLE `de-ibphilippov.de_shvetsov.week3_green_taxi_2022_clust`
+partition by lpep_pickup_date
+CLUSTER BY
+  PUlocationID
+  OPTIONS (
+    description = 'Clustered table')
+as (
+  select *, cast(lpep_pickup_datetime as date) lpep_pickup_date from `de-ibphilippov.de_shvetsov.week3_green_taxi_2022`)
 
 Q5
 SELECT distinct(PULocationID) FROM `de-ibphilippov.de_shvetsov.week3_green_taxi_2022` where cast(lpep_pickup_datetime as date) between "2022-06-01" and "2022-06-30"
