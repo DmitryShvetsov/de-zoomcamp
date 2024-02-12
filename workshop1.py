f274b97079cf95d0a61951752797130a2d2b569f
Shvetsov Dmitry <40828643+DmitryShvetsov@users.noreply.github.com>
2024-02-12 19:51:11 +0300

    Update workshop1.py

--- workshop1.py
+++ workshop1.py
@@ -15,3 +15,27 @@ for sqrt_value in generator:
     print(sqrt_value)
 
 print(sum1)
+
+
+# 3
+ages = 0
+
+def people_1():
+    for i in range(1, 6):
+        yield {"ID": i, "Name": f"Person_{i}", "Age": 25 + i, "City": "City_A"}
+
+for person in people_1():
+    print(person)
+    ages += person["Age"]
+
+
+def people_2():
+    for i in range(3, 9):
+        yield {"ID": i, "Name": f"Person_{i}", "Age": 30 + i, "City": "City_B", "Occupation": f"Job_{i}"}
+
+
+for person in people_2():
+    print(person)
+    ages += person["Age"]
+
+print(ages)
